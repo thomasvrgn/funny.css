ecfaabed544c7612ab03a4a2a4fb6b48850f611b
thomasvergne <thomas.vergne.contact@gmail.com>
2023-07-28 18:25:08 +0200

    feat: added tailwindCSS and postCSS configurations.

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+
+const plugin = require('tailwindcss/plugin');
+const colors = require('tailwindcss/colors');
+
+module.exports = {
+  content: [
+    './src/**/*.{html,js,ts,css}',
+    './preview/index.html'
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: {
+          DEFAULT: colors.green[400],
+          foreground: colors.green[950],
+        },
+        secondary: {
+          DEFAULT: colors.white,
+          foreground: colors.neutral[800],
+        },
+      }
+    },
+  },
+  plugins: [
+    plugin(function ({ addVariant }) {
+      addVariant('children', '& > *');
+    }),
+    require('@tailwindcss/forms')
+  ],
+}
